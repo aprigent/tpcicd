1de63bd4b56e757acea609a5dd0bb97d5c1810f5
aprigent <aprigent@mcafee-wifi.univ-lr.fr>
2024-11-13 10:26:17 +0100

    Depot 2 / PGCD et test

---
+++ src/my_arithmetic2_aprigent/pgcd.py
@@ -0,0 +1,5 @@
+def pgcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
---
+++ tests/test_pgcd.py
@@ -0,0 +1,23 @@
+import unittest
+from my_arithmetic2_aprigent.pgcd import pgcd
+#commentaire
+class TestPGCDFunction(unittest.TestCase):
+
+    def test_pgcd_1(self):
+        result = pgcd(48, 18)
+        self.assertEqual(result, 6)
+
+    def test_pgcd_2(self):
+        result = pgcd(60, 48)
+        self.assertEqual(result, 12)
+
+    def test_pgcd_3(self):
+        result = pgcd(17, 5)
+        self.assertEqual(result, 1)
+
+    def test_pgcd_4(self):
+        result = pgcd(1024, 256)
+        self.assertEqual(result, 256)
+
+if __name__ == '__main__':
+    unittest.main()
